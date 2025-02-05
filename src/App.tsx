77eccfdd6374dabeb69675492d75bdcf25b4b9eb
Induktr <nikitavoitenko2020@gmail.com>
2025-02-05 03:02:59 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
-import Projects from "./pages/Projects";
-import FAQ from "./pages/FAQ";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import BlogPost from './pages/BlogPost'
+import FAQ from './pages/FAQ'
+import FloatingBricks from './components/animations/FloatingBricks'
+import PageTransition from './components/animations/PageTransition'
+import './index.css'
 
-const queryClient = new QueryClient();
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <FloatingBricks />
+        <Header />
+        <main className="flex-grow pt-16">
+          <AnimatePresence mode="wait">
+            <PageTransition>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/projects/:id" element={<ProjectDetail />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="/faq" element={<FAQ />} />
+              </Routes>
+            </PageTransition>
+          </AnimatePresence>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default App
