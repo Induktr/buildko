3f8864802948434b01875877ed7ef13b55c73af3
Induktr <nikitavoitenko2020@gmail.com>
2025-02-22 11:07:42 +0200

    Add 1 ts file

---
+++ src/i18n/index.ts
@@ -0,0 +1,30 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import enTranslations from './locales/en.json'
+import ukTranslations from './locales/uk.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslations
+      },
+      uk: {
+        translation: ukTranslations
+      }
+    },
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'uk'],
+    interpolation: {
+      escapeValue: false
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage']
+    }
+  })
+
+export default i18n
